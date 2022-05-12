420890badbf9b543f992c3184d2068dfd9db3f4f
Rafael Amaral <rafaelpaulajr@gmail.com>
2022-05-12 11:54:28 -0300

    DTO

---
+++ backend/src/main/java/com/rafaelamaral/dscatalog/dto/CategoryDTO.java
@@ -0,0 +1,57 @@
+package com.rafaelamaral.dscatalog.dto;
+
+import com.rafaelamaral.dscatalog.entities.Category;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class CategoryDTO implements Serializable {
+
+    private static final Long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String name;
+
+    public CategoryDTO(){}
+
+    public CategoryDTO(Long id , String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public CategoryDTO(Category entity){
+        this.id = entity.getId();
+        this.name = entity.getName();
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CategoryDTO)) return false;
+        CategoryDTO that = (CategoryDTO) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
--- backend/src/main/java/com/rafaelamaral/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/rafaelamaral/dscatalog/services/CategoryService.java
@@ -1,5 +1,6 @@
 package com.rafaelamaral.dscatalog.services;
 
+import com.rafaelamaral.dscatalog.dto.CategoryDTO;
 import com.rafaelamaral.dscatalog.entities.Category;
 import com.rafaelamaral.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -15,8 +17,9 @@ public class CategoryService {
     private CategoryRepository repository;
 
     @Transactional(readOnly = true)
-    public List<Category> findAll(){
-        return repository.findAll();
+    public List<CategoryDTO> findAll(){
+        List<Category> categories = repository.findAll();
+        return categories.stream().map(X -> new CategoryDTO(X)).collect(Collectors.toList());
     }
 
 }
